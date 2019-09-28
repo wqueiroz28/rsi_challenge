f80a6ea828963f3cb7734cf9594cd4d655ef16b1
piresandre <andrepsantis@gmail.com>
2019-09-28 12:10:04 -0300

    feito priemiro commit

---
+++ src/main/java/br/com/challengersi/rsi/RsiApplication.java
@@ -0,0 +1,13 @@
+package br.com.challengersi.rsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/br/com/challengersi/rsi/controller/AuthController.java
@@ -0,0 +1,17 @@
+package br.com.challengersi.rsi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @RequestMapping("/users")
+    @ResponseBody
+    public String getUsers() {
+        return "{\"users\":[{\"name\":\"Lucas\", \"country\":\"Brazil\"}," +
+                "{\"name\":\"Jackie\",\"country\":\"China\"}]}";
+    }
+}
